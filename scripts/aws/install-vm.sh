d1969e6399e9dc94be93251b278c18bc2212c262
André Páscoa <andre@pascoa.org>
2024-05-23 18:43:27 +0100

    Fixed bug where the instrumentation did not take into account the blur algorithm instructions.

--- scripts/aws/install-vm.sh
+++ scripts/aws/install-vm.sh
@@ -18,6 +18,6 @@ scp -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAIR_PATH "$instrumentation_j
 scp -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAIR_PATH "$SCRIPT_DIR/config.sh" ec2-user@$(cat instance.dns):
 
 # Setup web server to start on instance launch.
-run_cmd="source /home/ec2-user/config.sh && java -cp /home/ec2-user/$webserver_jar -javaagent:/home/ec2-user/$instrumentation_jar=RequestAnalyzer:pt.ulisboa.tecnico.cnv.imageproc,pt.ulisboa.tecnico.cnv.raytracer:output pt.ulisboa.tecnico.cnv.webserver.WebServer > /home/ec2-user/webserver.log 2>&1"
+run_cmd="source /home/ec2-user/config.sh && java -cp /home/ec2-user/$webserver_jar -javaagent:/home/ec2-user/$instrumentation_jar=RequestAnalyzer:pt.ulisboa.tecnico.cnv.imageproc,boofcv,pt.ulisboa.tecnico.cnv.raytracer:output pt.ulisboa.tecnico.cnv.webserver.WebServer > /home/ec2-user/webserver.log 2>&1"
 cmd="echo \"$run_cmd\" | sudo tee -a /etc/rc.local; sudo chmod +x /etc/rc.local"
 ssh -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAIR_PATH ec2-user@$(cat instance.dns) $cmd
